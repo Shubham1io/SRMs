da1685ea14bbef5bbd2b564a3991776951d17f73
shubham <sk229215@gmail.com>
2025-02-09 20:46:49 +0530

    deleting

--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-self.add_button=Button(self.root,text="Save",font=("goudy old style",15,"bold"),bg="#2196f3",fg="white",cursor="hand2",command=self.add)
-        self.add_button.place(x=150,y=400,width=110,height=40)
